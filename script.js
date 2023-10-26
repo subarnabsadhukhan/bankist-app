b08b450942e7c99be9026383a722f1914c08a584
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-10-26 06:56:09 +0530

    calculate NET balance of the account

--- script.js
+++ script.js
@@ -93,3 +93,10 @@ const createUsernames = function (accounts) {
   });
 };
 createUsernames(accounts);
+
+// #E-4
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance}€`;
+};
+calcDisplayBalance(account1.movements);
