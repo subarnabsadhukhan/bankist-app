18e73438da902d9e327325f1faccc05000c22458
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-10-28 06:26:17 +0530

    implement close account feature

--- script.js
+++ script.js
@@ -175,3 +175,24 @@ btnTransfer.addEventListener("click", function (e) {
   inputTransferAmount.blur();
   inputTransferTo.blur();
 });
+// #E-9
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    // Delete Account
+    accounts.splice(index, 1);
+    // Hide UI
+    containerApp.style.opacity = 0;
+    // Change Welcome Message
+    labelWelcome.textContent = `Log in to get started`;
+  }
+  inputClosePin.value = inputCloseUsername.value = "";
+  inputClosePin.blur();
+  inputCloseUsername.blur();
+});
